14a950d96b3c052f1cc546e0734ded193db373c6
ttinonin <tinonin935@gmail.com>
2023-09-07 14:52:25 -0300

    Adicionando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LedComponent } from './led/led.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MobileSidebarComponent } from './mobile-sidebar/mobile-sidebar.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "iniciando/led", component: LedComponent }
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +20,15 @@ import { MobileSidebarComponent } from './mobile-sidebar/mobile-sidebar.componen
     HeaderComponent,
     LedComponent,
     SidebarComponent,
-    MobileSidebarComponent
+    MobileSidebarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
